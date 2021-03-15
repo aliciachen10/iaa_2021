345d810faa1654f2a9f57650719fedd658b564e4
Dan Zaratsian <dtz001@gmail.com>
2021-03-15 13:15:47 -0400

    updated gcloud syntax

--- session_01/dataproc_2_spark_submit.sh
+++ session_01/dataproc_2_spark_submit.sh
@@ -2,6 +2,6 @@ CLUSTER_NAME=iaa-gccluster
 PYSPARK_SCRIPT=spark_test_script.py
 
 # Submit pyspark job
-gcloud dataproc jobs submit pyspark -region us-central1 --cluster=$CLUSTER_NAME $PYSPARK_SCRIPT
+gcloud dataproc jobs submit pyspark --region us-central1 --cluster=$CLUSTER_NAME $PYSPARK_SCRIPT
 
 #ZEND
