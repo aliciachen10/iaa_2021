1cee36d19b8881af41a01d1e621386544206705c
Dan Zaratsian <dtz001@gmail.com>
2021-04-02 08:58:41 -0400

    Update python_joke.py

--- session_06/python_joke.py
+++ session_06/python_joke.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         data=json.dumps(payload))
     
     if r.status_code == 200:
-        print('[ INFO ] Success! {}'.format(r.content))
+        print('[ INFO ] Response: {}'.format(r.content))
         print('[ INFO ] Payload passed to Google Cloud Functions: {}'.format(payload))
     else:
         print('[ ERROR ] Status Code: {}. {}'.format(r.status_code, r.content))
